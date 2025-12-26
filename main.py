01a1f3e913997a56893997b28e6a397d8ae14005
Tigray52 <kamalenkoizabelko@gmail.com>
2025-12-26 16:32:33 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,17 @@
 import telebot
-from telebot import types
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv()  # Загружает .env файл
+# Создаем экземпляр бота
 bot = telebot.TeleBot(os.getenv('BOT_TOKEN'))
-
+# Функция, обрабатывающая команду /start
 @bot.message_handler(commands=["start"])
-def start(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    markup.add("✉️ Написать в поддержку")
-    bot.send_message(
-        message.chat.id,
-        "Здравствуйте! Я могу вам помочь?",
-        reply_markup=markup
-    )
-
-@bot.message_handler(func=lambda m: True)
+def start(m, res=False):
+    bot.send_message(m.chat.id, 'Я на связи. Напиши мне что-нибудь )')
+# Получение сообщений от юзера
+@bot.message_handler(content_types=["text"])
 def handle_text(message):
-    if message.text == "✉️ Написать в поддержку":
-        bot.send_message(message.chat.id, "✅ Кнопка работает! Функция в разработке.")
-    else:
-        bot.send_message(message.chat.id, "Пожалуйста, используйте кнопки меню")
-        bot.delete_message(message.chat.id, message.message_id)
-
-bot.polling(none_stop=True)
+    bot.send_message(message.chat.id, 'Вы написали: ' + message.text)
+# Запускаем бота
+bot.polling(none_stop=True, interval=0)
